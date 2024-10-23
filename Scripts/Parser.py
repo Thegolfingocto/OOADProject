2c5cae4b3203b5456daa1f164ed146855dadfad7
brsrikrishna <brsrikrishna@gmail.com>
2024-10-23 02:36:36 -0600

    Able to extract methods in Java

--- Scripts/Parser.py
+++ Scripts/Parser.py
@@ -1,13 +1,24 @@
 import os
+import re
 
 vecTypes = ["bool", "boolean", "int", "integer", "float", "double", "void", "string", "char"]
 
+
+def find_java_method_definitions(code):
+    # Regular expression to match Java method definitions
+    pattern = r'\b(?:public|private|protected)?\s*(?:static\s*)?(?:[\w<>\[\]]+\s+)+(\w+)\s*\([^)]*\)\s*\{'
+    
+    # Find all matches using re.findall
+    methods = re.findall(pattern, code)
+    
+    return methods
+
 def Parse(strPath):
     with open(strPath, "r") as f:
-        vecLines = f.readlines()
-
-    print(vecLines)
-
+        methods = find_java_method_definitions(f.read())
+    print(methods)
+#methods = find_java_method_definitions(java_code)
+#print(methods)
 
 
 if __name__ == "__main__":
